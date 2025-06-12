3cbc42ce9ff3091715073d88bef4127b8ee3479d
Ashish <ashish_b@hs.iitr.ac.in>
2025-06-12 18:57:52 +0530

    addes main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+# --- CONFIGURATION ---
+NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
+NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASS", "password")
